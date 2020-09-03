a72ebce4156f6eca675e0626c17c46d219ed0d00
andryushque <andrew.ustinov23@gmail.com>
2020-09-03 14:25:05 +0300

    Create: projects section

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  /*=== Isotope filter ===*/
+  let tabButton = $(".site-projects .tab-button");
+
+  tabButton.click(function (e) {
+    // console.log("Click test");
+    $(".site-projects .tab-button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $(".site-projects .grid").isotope({
+      filter: selector,
+    });
+
+    return false;
+  });
+
+  $("#btn-all").trigger("click");
+});
